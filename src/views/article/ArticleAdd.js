220889db8e7f546bc5665dd45123c964c2d61719
winner <1750110604@qq.com>
2019-11-09 18:03:36 +0800

    update

--- src/views/article/ArticleAdd.js
+++ src/views/article/ArticleAdd.js
@@ -7,16 +7,20 @@ import {
 	Input,
 	Button,
 	Upload,
-	Icon
+	Icon,
+	DatePicker,
+	Checkbox
 } from 'antd'
-
+import E from 'wangeditor'
 import { beforeUpload } from '@/utils/FormMethods'
 import { getBase64 } from '@/utils/base64'
+import '@/assets/css/article.scss'
 
+const { TextArea } = Input
 // 表单布局
 const formItemLayout = {
 	labelCol: { span: 4 },
-	wrapperCol: { span: 8 },
+	wrapperCol: { span: 13 },
 };
 
 
@@ -26,9 +30,14 @@ class ArticleAdd extends React.Component{
 		super(props);
 		this.state = {
 			loading: false,
+			editorContent: ''
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	componentDidMount () {
+		this.initWangEditor()
 	}
 
 	render () {
@@ -39,7 +48,7 @@ class ArticleAdd extends React.Component{
 		const uploadButton = (
 			<div>
 				<Icon type={this.state.loading ? 'loading' : 'plus'} />
-				<div className="ant-upload-text">Upload</div>
+				<div className="ant-upload-text">上传中...</div>
 			</div>
 		);
 
@@ -83,7 +92,65 @@ class ArticleAdd extends React.Component{
 								</Upload>
 							)}
 					</Form.Item>
-					<Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+					<Form.Item label="文章简介：">
+						{
+							getFieldDecorator('description', {
+								rules: [
+									{
+										required: true,
+										message: '请输入文章简介'
+									}
+								]
+							})(<TextArea rows={4} placeholder="请输入文章简介" />)}
+					</Form.Item>
+					<Form.Item label="文章内容：">
+						{
+							getFieldDecorator('content', {
+								rules: [
+									{
+										required: true,
+										message: '请输入文章内容'
+									}
+								]
+							})(
+								<div className="text-area" >
+									<div
+										ref="editorElemMenu"
+										style={{backgroundColor:'#f1f1f1',border:"1px solid #ccc"}}
+										className="editorElem-menu">
+									</div>
+									<div
+										style={{
+											padding:"0 10px",
+											height:300,
+											border:"1px solid #ccc",
+											overflow: "auto",
+											borderTop: "none"
+										}}
+										ref="editorElemBody"
+										className="editorElem-body">
+									</div>
+								</div>
+							)}
+					</Form.Item>
+					<Form.Item label="创建时间：">
+						{
+							getFieldDecorator('createTime', {
+								rules: [
+									{
+										required: true,
+										message: '请选择创建时间'
+									}
+								]
+							})(<DatePicker showTime placeholder="请选择创建时间" />)}
+					</Form.Item>
+					<Form.Item label="置顶：">
+						{
+							getFieldDecorator('top', {
+
+							})(<Checkbox>是否置顶</Checkbox>)}
+					</Form.Item>
+					<Form.Item wrapperCol={{ span: 4, offset: 6 }}>
 						<Button type="primary" htmlType="submit">
 							提交
 						</Button>
@@ -123,6 +190,47 @@ class ArticleAdd extends React.Component{
 			);
 		}
 	};
+
+	/**
+	 * 初始化富文本编辑器
+	 */
+	initWangEditor = () => {
+		const elemMenu = this.refs.editorElemMenu;
+		const elemBody = this.refs.editorElemBody;
+		const editor = new E(elemMenu,elemBody)
+		// 使用 onchange 函数监听内容的变化，并实时更新到 state 中
+		editor.customConfig.onchange = html => {
+			console.log(editor.txt.html())
+			this.setState({
+				// editorContent: editor.txt.text()
+				editorContent: editor.txt.html()
+			})
+		}
+		editor.customConfig.menus = [
+			'head',  // 标题
+			'bold',  // 粗体
+			'fontSize',  // 字号
+			'fontName',  // 字体
+			'italic',  // 斜体
+			'underline',  // 下划线
+			'strikeThrough',  // 删除线
+			'foreColor',  // 文字颜色
+			'backColor',  // 背景颜色
+			'link',  // 插入链接
+			'list',  // 列表
+			'justify',  // 对齐方式
+			'quote',  // 引用
+			'emoticon',  // 表情
+			'image',  // 插入图片
+			'table',  // 表格
+			'video',  // 插入视频
+			'code',  // 插入代码
+			'undo',  // 撤销
+			'redo'  // 重复
+		]
+		editor.customConfig.uploadImgShowBase64 = true
+		editor.create()
+	}
 
 }
 
